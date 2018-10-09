721e7c31e85fc6d9759aaaa5625d8d0edd0c8916
Manu Pedreira <mpedreiraceballos@gmail.com>
2018-10-09 10:32:09 +0200

    [ADD] First commit: adds the loader and the readme

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # javascript_library_loader
+
+This library manages the asynchronous loading of Javascript modules.
+
+It loads a module into the window object that can be called from the browser console.
+
+# Loads a single module and executes the callback one the loading is finished
+
+# or throws an error
+
+window.loader.load('url_of_the_module', callback());
+
+# Loads several modules following the order given in the array, in case any of
+
+# them is dependent of the previous ones, throwing an error and stopping the flux
+
+# in case of one of the modules not loading, and executes the callback once all the
+
+# modules have been loaded
+
+window.loader.loadAll(array_of_modules[], callback());
--- loader.js
+++ loader.js
@@ -1,18 +1,62 @@
-window.loader = {        
-    _success(library) {
-        console.log(`La librería ${library} se ha cargado correctamente.`);
-    },
-    _error(library, error) {
-        console.log(`
-            La librería ${library} no se ha podido cargar.
-            Mensaje: ${error.message}.
-        `);
-    },
-    async load(library, callback = this._success) {
-        console.log('Holi');
-        callback();
-    },
-    async loadAll(libraries, callback) {
-        console.log('Holis');
-    },
-};
+function load() {
+    window.loader = {
+        _start(library) {
+            console.log(`Starting loading ${library}`);
+        },   
+        _success(library) {
+            console.log(`The library ${library} loaded correctly.`);
+        },
+        _error(library, error) {
+            console.log(`
+                The library ${library} has not being loaded.
+                Error message: ${error.message}.
+            `);
+        },
+        async load(library, callback) {
+            this._start(library);
+            return await fetch(library)
+                .then(response => {
+                    let error;
+                    if (response.ok) {
+                        const contentType = response.headers.get('Content-Type') || '';
+                    
+                        if (contentType.includes('application/javascript')) {
+                            this._success(library);
+                            if (!!callback && typeof callback === 'function') callback();
+                            return Promise.resolve(response.body);
+                        }
+                    
+                        throw new Error('Invalid content type: ' + contentType);
+                    }
+                
+                    if (response.status == 404) {
+                        throw new Error('Page not found: ' + url);
+                    }
+
+                    throw new Error('HTTP error: ' + response.status);
+                }).catch(error => {
+                    this._error(library, error);
+                    return Promise.reject(error);
+                });
+        },
+        async loadAll(libraries, callback) {
+            const callbackCall = () => {
+                console.log('All libraries have been loaded');
+                if (!!callback && typeof callback === 'function') callback();
+            };
+            let fnCalls = libraries.map(library => () => this.load(library));
+            fnCalls.push(callbackCall);
+            fnCalls.reduce(async (promise, fn, index, array) => {
+                return promise.then(response => {
+                    if (response == false) return false;
+                    return fn();
+                }).catch(() => false);
+            }, Promise.resolve());
+            
+        },
+    };
+
+    const event = new Event('loader.loaded');
+    window.dispatchEvent(event);
+}
+window.onload = load;
